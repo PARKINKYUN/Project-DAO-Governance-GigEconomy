10bf92c6823bbe8ab670b98249c5fb0a403ddb3c
PARKINKYUN <104402029+PARKINKYUN@users.noreply.github.com>
2022-09-05 10:42:32 +0900

    routing structure

---
+++ server/routes/main.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/main")
+
+router.get("/", controller.findAll);
+
+module.exports = router;
