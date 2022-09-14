981a793cdc8c60f1b1401ad6a51917f46a5ec8b2
JSND-OJ00 <jsnd.oj@gmail.com>
2022-09-14 09:41:44 +0900

    connect to backend

--- client/src/Page/FindOrder.js
+++ client/src/Page/FindOrder.js
@@ -1,11 +1,24 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import Order from "../components/Order";
+import axios from "axios";
 import withRoot from "../withRoot";
 import styles from "../css/FindOrder.module.css";
-import * as React from "react";
-import { Link as RouterLink } from "react-router-dom";
 import Link from "@mui/material/Link";
-import OrderList from "../components/OrderList";
 
 function FindOrder() {
+  const [orders, setOrders] = useState([]);
+
+  const getOrders = async () =>
+    await axios.get("http://localhost:4000/orders", {}).then((res) => {
+      setOrders(res.data);
+    });
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
   return (
     <div className={styles.main}>
       <div className={styles.orderlist}>
@@ -19,11 +32,24 @@ function FindOrder() {
         </div>
         <div className={styles.container}>
           <div className={styles.item}>
-            <OrderList
-              title={"Dave"}
-              text={"Hi"}
-              src={require("../img/worker4.jpg")}
-            />
+            {orders.map((order) => {
+              const {
+                _id,
+                client_id,
+                title,
+                category,
+                deadline,
+                compensation,
+              } = order;
+              <Order
+                id={_id}
+                client_id={client_id}
+                title={title}
+                category={category}
+                deadline={deadline}
+                compensation={compensation}
+              />;
+            })}
           </div>
         </div>
       </div>
--- client/src/Page/OrderInfo.js
+++ client/src/Page/OrderInfo.js
@@ -2,8 +2,88 @@ import withRoot from "../withRoot";
 import styles from "../css/OrderInfo.module.css";
 import Profile from "../components/Profile";
 import Tap from "../components/Tap";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
+import OfferList from "../components/Offer";
+import Order from "../components/Order";
 
-function OrderInfo() {
+function OrderInfo({ _id }) {
+  const [order, setOrder] = useState({});
+  const [offers, setOffers] = useState([]);
+  const [offerIdx, setOfferIdx] = useState(null);
+
+  // 오더 정보 불러오기
+  const getOrder = async () =>
+    axios.get(`http://localhost:4000/order_info/${id}`).then((res) => {
+      setOrder(res.data);
+    });
+
+  //
+
+  // 클라이언트가 워커의 제안을 선택하여 오더 시작
+  const clientStartOrder = async () => {
+    offerIdx !== null
+      ? await axios.patch(`http://localhost:4000/order_info/${id}/client_start`)
+      : console.log("오퍼를 선택해주세요.");
+  };
+
+  // 워커가 direct_order를 통해 생성된 오더 시작
+  const workerStartOrder = async () => {
+    await axios.patch(`http://localhost:4000/order_info/${id}/worker_start`);
+  };
+
+  useEffect(() => {
+    getOrder();
+    setOffers(order.offers);
+  }, []);
+
+  const handleDirectOrder = () => {
+    if (order.status == "pending") {
+      if (user == "client") {
+        return <div>오더수락버튼</div>;
+      }
+      if (user == "worker") {
+        return <div>오더삭제버튼</div>;
+      }
+    }
+    if (order.status == "ongoing") {
+      if (user == "client") {
+        return null;
+      }
+      if (user == "worker") {
+        return <div>오더연장버튼, 오더완료버튼, 오더취소버튼</div>;
+      }
+    }
+  };
+  const handlePublicOrder = () => {
+    if (order.status == "pending") {
+      if (user == "client") {
+        return (
+          <div>
+            <OfferList title={offers.title} />
+          </div>
+        );
+      }
+      if (user == "worker") {
+        return (
+          <div>
+            <OfferList />
+            오퍼등록버튼
+          </div>
+        );
+      }
+    }
+    if (order.status == "ongoing") {
+      if (user == "client") {
+        return null;
+      }
+      if (user == "worker") {
+        return <div>오더연장버튼, 오더완료버튼, 오더취소버튼</div>;
+      }
+    }
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.profile}>
@@ -14,6 +94,7 @@ function OrderInfo() {
         <div className={styles.reviewBox}>
           <div>Order Info</div>
         </div>
+        {order.direct_order == true ? handleDirectOrder() : handlePublicOrder()}
         <div>
           <Tap />
         </div>
