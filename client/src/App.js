b7951fafaf3393499a0d2b2a5c6c73acc21bb936
PARKINKYUN <104402029+PARKINKYUN@users.noreply.github.com>
2022-09-14 21:53:02 +0900

    ......

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import Governance from "./Page/Governance";
 import Main from "./Page/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
-import ClientProfile from "./Page/ClientProfile";
+import ClientInfo from "./Page/ClientInfo";
 
 function App() {
   const [token, setToken] = useState('');
@@ -35,7 +35,7 @@ function App() {
         <Route path="/signin" element={<SignIn setToken={setToken} setUserInfo={setUserInfo} setIsWorker={setIsWorker} />} />
         <Route path="/findworker" element={<FindWorker />} />
         <Route path="/findorder" element={<FindOrder />} />
-        <Route path="/clientprofile" element={<ClientProfile />} />
+        <Route path="/clientprofile" element={<ClientInfo token={token} userInfo={userInfo} setUserInfo={setUserInfo} />} />
         <Route path="/workerprofile" element={<WorkerProfile />} />
         <Route path="/orderinfo" element={<OrderInfo />} />
         <Route path="/createorder" element={<CreateOrder />} />
