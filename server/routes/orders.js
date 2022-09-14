ff3931e6773e2c7b074e7743ab4e4e6807959603
JSND-OJ00 <jsnd.oj@gmail.com>
2022-09-14 16:19:35 +0900

    wip

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -7,14 +7,15 @@ router.get("/order_info/:id", controller.order_info); //오더 조회
 router.post("/new_order", controller.new_order); //새로운 오더 생성
 router.post("/direct_order/:id", controller.direct_order); //워커에게 직접 의뢰하기
 
+// status pending only
 router.patch("/order_info/:id/edit", controller.edit_order); //오더 내용 변경
 router.patch("/order_info/:id/make_offer", controller.make_offer); //워커가 pending 상태인 오더에 제안 등록
 router.patch("/order_info/:id/client_start", controller.clientStart); //오더 시작
 router.patch("/order_info/:id/worker_start", controller.workerStart); //오더 시작
+router.patch("/order_info/:id/remove", controller.remove); //오더 삭제
 
 router.patch("/order_info/:id/extend", controller.extend); //오더 연장
 router.patch("/order_info/:id/cancel", controller.cancel); //오더 취소
 router.patch("/order_info/:id/finish", controller.finish); //오더 완료
-router.patch("/order_info/:id/remove", controller.remove); //오더 삭제
 
 module.exports = router;
