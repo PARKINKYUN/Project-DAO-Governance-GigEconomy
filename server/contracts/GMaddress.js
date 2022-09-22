9b714134cb8700228092ef7a7df1a050aa076ce6
PARKINKYUN <104402029+PARKINKYUN@users.noreply.github.com>
2022-09-23 08:30:38 +0900

    컨트랙트 v 1.0

---
+++ server/contracts/GMaddress.js
@@ -0,0 +1,3 @@
+const GMaddress = "0x453eE7F3cD9A1F802c4DF2a220Db6f23b70EeeA7";
+
+module.exports = GMaddress;
---
+++ server/contracts/GTaddress.js
@@ -0,0 +1,3 @@
+const GTaddress = "0x56fd37Ddc04bCDaBA577c9a4aA47B4c7ea10BA21";
+
+module.exports = GTaddress;
