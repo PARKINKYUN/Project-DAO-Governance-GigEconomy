cd9c84ea343d985944f4bc9e48015fcca48ee80a
kimsj3592 <kimsj3592@naver.com>
2022-09-26 11:52:23 +0900

    리뷰 보상, pending 토큰지불

--- server/controllers/reviews.controller.js
+++ server/controllers/reviews.controller.js
@@ -1,6 +1,14 @@
 const reviewmodel = require("../models/review.model"); // 외부 모듈 가져오기, 파라미터로 추가할 모듈의 파일 경로값을 받음
+const workerModel = require("../models/worker.model");
+
 const jwt = require("jsonwebtoken"); // 클라이언트와 서버 사이 통신시 권한을 인가하기 위해 사용하는 토큰
 
+const Web3 = require("web3");
+const GTabi = require("../contracts/GTabi");
+const GTaddress = require("../contracts/GTaddress");
+
+const web3 = new Web3(process.env.RPCURL);
+const gt = new web3.eth.Contract(GTabi, GTaddress);
 module.exports = {
   // 리뷰 작성
   newreview: async (req, res) => {
@@ -24,10 +32,30 @@ module.exports = {
             order_id: req.body.order_id,
             content: req.body.content,
           };
+          //후기 작성 리워드 보상 컨트랙트 작성 필
+
+          const data = gt.methods.rewardReview(userInfo.address).encodeABI();
+          const rawTransaction = { to: GTaddress, gas: 1000000, data: data };
+          const signedTX = await web3.eth.accounts.signTransaction(
+            rawTransaction,
+            process.env.ADMIN_WALLET_PRIVATE_KEY
+          );
+          const sendingTX = await web3.eth.sendSignedTransaction(
+            signedTX.rawTransaction
+          );
+          console.log("Token 전송 트랜잭션: ", sendingTX);
+
+          // 리워드 전송 후 worker 잔액 확인
+          const workerBalance = await gt.methods
+            .balanceOf(userInfo.address)
+            .call();
+          await workerModel.setToken(userInfo.address, workerBalance);
           const inputReview = await reviewmodel.saveReview(newReview);
           console.log("똑똑! 새로운 review가 저장되었습니다.", inputReview);
 
-          return res.status(200).send({ data: inputReview, message: "Created new review" });
+          return res
+            .status(200)
+            .send({ data: inputReview, message: "Created new review" });
         }
       }
     } catch (err) {
@@ -53,7 +81,9 @@ module.exports = {
         if (!userInfo) {
           return res.status(404).send({ data: null, message: "Invalid token" });
         } else {
-          const reviewInfo = await reviewmodel.getReviewByWorkerId(req.params.worker_id);
+          const reviewInfo = await reviewmodel.getReviewByWorkerId(
+            req.params.worker_id
+          );
           return res
             .status(200)
             .send({ data: reviewInfo, message: "Searching success" });
@@ -147,7 +177,7 @@ module.exports = {
           return res.status(404).send({ data: null, message: "Invalid token" });
         } else {
           const deletereview = await reviewmodel.removeReview(
-            req.body.review_id,
+            req.body.review_id
           );
           console.log("review 삭제가 완료되었습니다.", deletereview);
           return res
@@ -168,9 +198,7 @@ module.exports = {
   recentReview: async (req, res) => {
     try {
       const recentReview = await reviewmodel.recentReview();
-      return res
-        .status(200)
-        .send({ data: recentReview, message: "ok" });
+      return res.status(200).send({ data: recentReview, message: "ok" });
     } catch (err) {
       // console.log(err);
       res.status(400).send({
